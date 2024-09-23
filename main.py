c79ed0742bdda8a5adb19ebde8d1fd561abe6f75
Atriyo <atriyo.gogol@gmail.com>
2024-09-24 01:12:42 +0530

    modified the following files for model evaluation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainin
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -36,6 +37,15 @@ try:
     logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
     model_training = ModelTrainerTrainingPipeline()
     model_training.main()
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<\n\nx===================x")
+except Exception as e:
+    raise e
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+    model_evaluation = ModelEvaluationPipeline()
+    model_evaluation.main()
     logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<\n\nx===================x")
 except Exception as e:
     raise e
