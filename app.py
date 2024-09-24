3e86e6ab715d13541468fa2fc6ddb5035abef7f5
Atriyo <atriyo.gogol@gmail.com>
2024-09-24 17:57:20 +0530

    modeified the app files for integrating it with streamlist

--- app.py
+++ app.py
@@ -6,30 +6,38 @@ from fastapi.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
 from fastapi.responses import Response
 from textSummarizer.pipeline.prediction import PredictionPipeline
+from pydantic import BaseModel
 
 text:str = "What is Text Summarization?"
 
 app = FastAPI()
 
+class Payload(BaseModel):
+    input_text: str
+    min_length: int
+    max_length: int
+
+
+
 @app.get("/", tags=["authentication"])
 async def index():
     return RedirectResponse(url="/docs")
 
-@app.get("/train")
-async def training():
-    try:
-        os.system("python main.py")
-        return Response("Training successful !!")
+# @app.get("/train")
+# async def training():
+#     try:
+#         os.system("python main.py")
+#         return Response("Training successful !!")
 
-    except Exception as e:
-        return Response(f"Error Occurred! {e}")
+#     except Exception as e:
+#         return Response(f"Error Occurred! {e}")
 
 @app.post("/predict")
-async def predict_route(text):
+async def predict_route(input:Payload):
     try:
 
         obj = PredictionPipeline()
-        text = obj.predict(text)
+        text = obj.predict(input.input_text, input.min_length, input.max_length)
         return text
     except Exception as e:
         raise e
--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -1,6 +1,7 @@
 from textSummarizer.config.configuration import ConfigurationManager
 from transformers import AutoTokenizer
 from transformers import pipeline
+import torch
 
 
 class PredictionPipeline:
@@ -9,11 +10,16 @@ class PredictionPipeline:
 
 
     
-    def predict(self, text):
+    def predict(self, text, min_text, max_text):
+        print(torch.cuda.is_available())
+        device = 0 if torch.cuda.is_available() else -1
+        print(device)
         tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
-        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
+        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": max_text, "min_length":min_text}
+        print(gen_kwargs)
+        # gen_kwargs = kwargs
 
-        pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer)
+        pipe = pipeline("summarization", model=self.config.model_path, tokenizer=tokenizer, device=device)
 
         print("Dialogue:")
         print(text)
